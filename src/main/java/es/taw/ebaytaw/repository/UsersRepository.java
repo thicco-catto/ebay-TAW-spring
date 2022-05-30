5e498ab6dc01270ea93f1821c2d4bd385bdc1e17
cristobalmartinarenas <crimarare@uma.es>
2022-05-30 12:08:01 +0200

    Added filter to userRepository

--- src/main/java/es/taw/ebaytaw/repository/UsersRepository.java
+++ src/main/java/es/taw/ebaytaw/repository/UsersRepository.java
@@ -19,5 +19,6 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
     //Query("SELECT u FROM Users u WHERE u.name like '%  :nombreUsuario %' ")
     //public List<UserDTO> listarUsuariosFiltrado(String nombreUsuario, String orderBy);
 
-
+    @Query("select u from Users u where (:rol is null or u.rol = :rol) and (:email is null or u.email like :email) and (:username is null or u.username like :username) and (:email is null or u.email like :email) and (:name is null or u.name like :name) and (:surname is null or u.surname like :surname) and (:gender is null or u.gender = :gender) and (:street is null or u.street like :street) and (:number is null or u.number = :number) and (:city is null or u.city like :city) and (:region is null or u.region like :region) and (:postalCode is null or u.postalCode = :postalCode)")
+    public Users getUsers(String rol, String username, String email, String name, String surname, String gender, String street, Integer number, String city, String region, Integer postalCode);
 }
